a692aa269a9deb7b28fa772608adf09825955e89
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-09-26 15:01:12 +0300

    Run classifier usage checkers after all imports are resolved
    Otherwise the diagnostic was not reported on an unused import of a deprecated
    class in diagnostic tests, see the test case

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/LazyTopDownAnalyzer.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/LazyTopDownAnalyzer.kt
@@ -206,9 +206,9 @@ class LazyTopDownAnalyzer(
 
         bodyResolver.resolveBodies(c)
 
-        ClassifierUsageChecker.check(declarations, trace, classifierUsageCheckers)
-
         resolveImportsInAllFiles(c)
+
+        ClassifierUsageChecker.check(declarations, trace, classifierUsageCheckers)
 
         return c
     }
